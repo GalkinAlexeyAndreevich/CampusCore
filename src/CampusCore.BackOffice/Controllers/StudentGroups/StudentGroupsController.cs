dd87b90026eea5c8d11299de322bde15bbb91b92
GalkinAlexeyAndreevich <sir.methews@gmail.com>
2026-03-02 13:57:08 +0300

    feat: add front template

---
+++ src/CampusCore.BackOffice/Controllers/StudentGroups/StudentGroupsController.cs
@@ -0,0 +1,33 @@
+﻿using CampusCore.Domain.Products;
+using CampusCore.Domain.Services;
+using CampusCore.Tools.Types.Results;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CampusCore.BackOffice.Controllers.StudentGroups;
+
+public class StudentsController(IProductsService productsService) : AppController
+{
+	[HttpPost("student-group/save")]
+	public Result SaveProducts([FromBody] ProductBlank productBlank)
+	{
+		return productsService.SaveProduct(productBlank);
+	}
+
+	[HttpGet("student-group/get_page")]
+	public Page<Product> GetStudentsPage([FromQuery] Int32 page, [FromQuery] Int32 countInPage)
+	{
+		return productsService.GetProductsPage(page, countInPage);
+	}
+
+	[HttpGet("student-group/get_by_id")]
+	public Product? GetStudent([FromQuery] Guid productId)
+	{
+		return productsService.GetProduct(productId);
+	}
+
+	[HttpGet("student-group/mark_student_as_removed")]
+	public Result MarkStudentAsRemoved([FromQuery] Guid productId)
+	{
+		return productsService.MarkProductAsRemoved(productId);
+	}
+}

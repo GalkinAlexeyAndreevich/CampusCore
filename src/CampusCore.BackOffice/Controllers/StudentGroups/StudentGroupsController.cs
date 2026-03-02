7b79f9ca39d2b4d3037f1d9d744fdad69a17be25
GalkinAlexeyAndreevich <sir.methews@gmail.com>
2026-03-02 17:48:07 +0300

    feat: add student group controller

--- src/CampusCore.BackOffice/Controllers/StudentGroups/StudentGroupsController.cs
+++ src/CampusCore.BackOffice/Controllers/StudentGroups/StudentGroupsController.cs
@@ -1,33 +1,34 @@
 ﻿using CampusCore.Domain.Products;
 using CampusCore.Domain.Services;
+using CampusCore.Domain.StudentGroups;
 using CampusCore.Tools.Types.Results;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CampusCore.BackOffice.Controllers.StudentGroups;
 
-public class StudentsController(IProductsService productsService) : AppController
+public class StudentsController(IStudentGroupsService studentGroupsService) : AppController
 {
-	[HttpPost("student-group/save")]
-	public Result SaveProducts([FromBody] ProductBlank productBlank)
+	[HttpPost("student-groups/save")]
+	public Result SaveStudents([FromBody] StudentGroupBlank studentGroupBlank)
 	{
-		return productsService.SaveProduct(productBlank);
+		return studentGroupsService.SaveStudentGroup(studentGroupBlank);
 	}
 
-	[HttpGet("student-group/get_page")]
-	public Page<Product> GetStudentsPage([FromQuery] Int32 page, [FromQuery] Int32 countInPage)
+	[HttpGet("student-groups")]
+	public StudentGroup[] GetStudentsPage()
 	{
-		return productsService.GetProductsPage(page, countInPage);
+		return studentGroupsService.GetAllStudentGroups();
 	}
 
-	[HttpGet("student-group/get_by_id")]
-	public Product? GetStudent([FromQuery] Guid productId)
+	[HttpGet("student-groups/get_by_id")]
+	public StudentGroup? GetStudent([FromQuery] Guid groupId)
 	{
-		return productsService.GetProduct(productId);
+		return studentGroupsService.GetStudentGroup(groupId);
 	}
 
-	[HttpGet("student-group/mark_student_as_removed")]
-	public Result MarkStudentAsRemoved([FromQuery] Guid productId)
+	[HttpGet("student-groups/mark_as_deleted")]
+	public Result MarkStudentAsRemoved([FromQuery] Guid groupId)
 	{
-		return productsService.MarkProductAsRemoved(productId);
+		return studentGroupsService.MarkStudentGroupAsDeleted(groupId);
 	}
 }
--- src/CampusCore.Services/ServiceConfigurator.cs
+++ src/CampusCore.Services/ServiceConfigurator.cs
@@ -1,4 +1,10 @@
 ﻿using CampusCore.Domain.Services;
+using CampusCore.Services.Products;
+using CampusCore.Services.Products.Repositories;
+using CampusCore.Services.Products.Repositories.Interfaces;
+using CampusCore.Services.StudentGroups;
+using CampusCore.Services.StudentGroups.Repositories;
+using CampusCore.Services.StudentGroups.Repositories.Interfaces;
 using CampusCore.Services.Students;
 using CampusCore.Services.Students.Repositories;
 using CampusCore.Services.Students.Repositories.Interfaces;
@@ -10,9 +16,14 @@ public static class ServiceConfigurator
 {
 	public static IServiceCollection AddServices(this IServiceCollection collection)
 	{
+		collection.AddSingleton<IProductsService, ProductsService>();
+		collection.AddSingleton<IProductsRepository, ProductsRepository>();
+		
 		collection.AddSingleton<IStudentsService, StudentsService>();
-
 		collection.AddSingleton<IStudentsRepository, StudentsRepository>();
+
+		collection.AddSingleton<IStudentGroupsService, StudentGroupsService>();
+		collection.AddSingleton<IStudentGroupsRepository, StudentGroupsRepository>();
 
 		return collection;
 	}

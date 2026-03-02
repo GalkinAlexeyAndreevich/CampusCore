06e31f8a0ec83305bd45528dea347c2ca7f3c2c6
GalkinAlexeyAndreevich <sir.methews@gmail.com>
2026-03-02 17:24:15 +0300

    update: format code student converter

--- src/CampusCore.Services/Students/Repositories/Converters/StudentsConverter.cs
+++ src/CampusCore.Services/Students/Repositories/Converters/StudentsConverter.cs
@@ -6,29 +6,39 @@ namespace CampusCore.Services.Students.Repositories.Converters;
 
 internal static class StudentsConverter
 {
-	internal static Student[] ToStudents(this StudentDb[] studentDbs) => [.. studentDbs.Select(ToStudent)];
-	
-	internal static Student ToStudent(this StudentDb studentDb)
-	{
-		return new Student(studentDb.Id, studentDb.LastName, studentDb.FirstName, studentDb.Patronymic, studentDb.Gender, studentDb.Age, studentDb.AverageGrade, studentDb.SpecialNotes, studentDb.GroupId);
-	}
+    internal static Student[] ToStudents(this StudentDb[] studentDbs) => [.. studentDbs.Select(ToStudent)];
 
-	internal static StudentDb ToStudentDb(this NpgsqlDataReader reader)
-	{
-		return new StudentDb(
-			reader.GetGuid(reader.GetOrdinal("id")),
-			reader.GetString(reader.GetOrdinal("last_name")),
-			reader.GetString(reader.GetOrdinal("first_name")),
-			reader.IsDBNull(reader.GetOrdinal("patronymic")) 
-				? null
-				:reader.GetString(reader.GetOrdinal("patronymic")),
-			reader.GetString(reader.GetOrdinal("gender")),
-			reader.GetInt32(reader.GetOrdinal("age")),
-			reader.GetDecimal(reader.GetOrdinal("average_grade")),
-			reader.IsDBNull(reader.GetOrdinal("special_notes"))
-				? null
-				: reader.GetFieldValue<String[]>(reader.GetOrdinal("special_notes")),
-			reader.GetGuid(reader.GetOrdinal("group_id"))
-		);
-	}
+    internal static Student ToStudent(this StudentDb studentDb)
+    {
+        return new Student(
+            studentDb.Id,
+            studentDb.LastName,
+            studentDb.FirstName,
+            studentDb.Patronymic,
+            studentDb.Gender,
+            studentDb.Age,
+            studentDb.AverageGrade,
+            studentDb.SpecialNotes,
+            studentDb.GroupId
+        );
+    }
+
+    internal static StudentDb ToStudentDb(this NpgsqlDataReader reader)
+    {
+        return new StudentDb(
+            reader.GetGuid(reader.GetOrdinal("id")),
+            reader.GetString(reader.GetOrdinal("last_name")),
+            reader.GetString(reader.GetOrdinal("first_name")),
+            reader.IsDBNull(reader.GetOrdinal("patronymic"))
+                ? null
+                : reader.GetString(reader.GetOrdinal("patronymic")),
+            reader.GetString(reader.GetOrdinal("gender")),
+            reader.GetInt32(reader.GetOrdinal("age")),
+            reader.GetDecimal(reader.GetOrdinal("average_grade")),
+            reader.IsDBNull(reader.GetOrdinal("special_notes"))
+                ? null
+                : reader.GetFieldValue<String[]>(reader.GetOrdinal("special_notes")),
+            reader.GetGuid(reader.GetOrdinal("group_id"))
+        );
+    }
 }

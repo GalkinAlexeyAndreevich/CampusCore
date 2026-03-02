ef46578c96c3a543b5dd9abcc73c48c781c81194
GalkinAlexeyAndreevich <sir.methews@gmail.com>
2026-03-02 15:59:40 +0300

    feat: add model and interface for student

---
+++ src/CampusCore.Services/Students/Repositories/Interfaces/IStudentsRepository.cs
@@ -0,0 +1,12 @@
+﻿using CampusCore.Domain.Students;
+using CampusCore.Tools.Types.Results;
+
+namespace CampusCore.Services.Students.Repositories.Interfaces;
+
+public interface IStudentsRepository
+{
+	Result SaveStudent(StudentBlank studentBlank);
+	Student[] GetAllStudents();
+	Student? GetStudent(Guid studentId);
+	Result MarkStudentAsDeleted(Guid studentId);
+}
---
+++ src/CampusCore.Services/Students/Repositories/Models/StudentDb.cs
@@ -0,0 +1,24 @@
+﻿namespace CampusCore.Services.Students.Repositories.Models;
+
+public class StudentDb(
+    Guid id,
+    String lastName,
+    String firstName,
+    String? patronymic,
+    String gender,
+    Int32 age,
+    decimal averageGrade,
+    String[]? specialNotes,
+    Guid groupId
+)
+{
+    public Guid Id { get; set; } = id;
+    public String LastName { get; set; } = lastName;
+    public String FirstName { get; set; } = firstName;
+    public String? Patronymic { get; set; } = patronymic;
+    public String Gender { get; set; } = gender;
+    public Int32 Age { get; set; } = age;
+    public decimal AverageGrade { get; set; } = averageGrade;
+    public String[]? SpecialNotes { get; set; } = specialNotes;
+    public Guid GroupId { get; set; } = groupId;
+}
